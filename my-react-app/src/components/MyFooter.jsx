239f951c1364b49b931592efb41bf5a2fdc8e06e
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-04-25 12:52:07 +0300

    smol edits

--- my-react-app/src/components/MyFooter.jsx
+++ my-react-app/src/components/MyFooter.jsx
@@ -63,5 +63,5 @@ const MyFooter = () => ( <Container>
 </Col>
 </Row>
 </Container>
-)
+);
 export default MyFooter
--- my-react-app/src/components/MyLoader.jsx
+++ my-react-app/src/components/MyLoader.jsx
@@ -1,5 +1,5 @@
 import { Spinner } from 'react-bootstrap'
 
-const Loading = () =>   <Spinner animation="border" variant="light" />
+const MyLoader = () =>   <Spinner animation="border" variant="light" />
 
-export default Loading
+export default MyLoader
