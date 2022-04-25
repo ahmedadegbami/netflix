c6609c2194994b66270666e8f2312abbe8db346b
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-04-25 12:34:23 +0300

    edits

--- my-react-app/src/components/MyFooter.jsx
+++ my-react-app/src/components/MyFooter.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import { Col, Row, Container, Button } from "react-bootstrap"
 
 const MyFooter = <Container>
-    <Row>
-      <Row>
+    <Row md={12} className="justify-content-md-center">
+      <Row md={2}>
 <Col>
   <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Audio and Subtitles</a>
 </Col>
@@ -17,7 +17,7 @@ const MyFooter = <Container>
   <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Gift Cards</a>
 </Col>
 </Row>
-<Row>
+<Row md={2}>
 <Col>
   <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Media Center</a>
 </Col>
@@ -28,7 +28,7 @@ const MyFooter = <Container>
   <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Jobs</a>
 </Col>
 </Row>
-<Row>
+<Row md={2}>
 <Col>
   <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Terms of Use</a>
 </Col>
@@ -39,7 +39,7 @@ const MyFooter = <Container>
   <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">CarLegal Noticesds</a>
 </Col>
 </Row>
-<Row>
+<Row md={2}>
 <Col>
   <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Cookie Preferences</a>
 </Col>
@@ -51,16 +51,16 @@ const MyFooter = <Container>
 </Col>
 </Row>
 </Row>
-<Row>
+<Row className="justify-content-md-center">
 <Col md={12}>
   <br />
   <Button variant="outline-light">Service Code</Button>{' '}
 </Col>
-<div class="col-12">
+<Col md={12}>
   <p id="footer-text" class="">
     copy 1997-2019 Netflix, Inc.
   </p>
-</div>
+</Col>
 </Row>
 </Container>
 export default MyFooter
