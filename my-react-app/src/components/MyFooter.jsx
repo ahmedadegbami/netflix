bef315b90627d36e0c745eb779e877d6141c8c6c
MadaraBule <100915562+MadaraBule@users.noreply.github.com>
2022-04-25 11:54:37 +0300

    Update MyFooter.jsx

---
+++ my-react-app/src/components/MyFooter.jsx
@@ -0,0 +1,60 @@
+import React from "react"
+import { Col, Row, Container } from "react-bootstrap"
+
+const MyFooter = <Container>
+    <Row>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Audio and Subtitles</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Audio Description</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Help Center</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Gift Cards</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Media Center</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Investor Relations</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Jobs</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Terms of Use</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Privacy</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">CarLegal Noticesds</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Cookie Preferences</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Corporate Information</a>
+</Col>
+<Col>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Contact Us</a>
+</Col>
+</Row>
+<div class="row">
+<div class="col-12">
+  <br />
+  <button type="button" class="btn btn-outline-primary">
+    Service Code
+  </button>
+</div>
+<div class="col-12">
+  <p id="footer-text" class="">
+    copy 1997-2019 Netflix, Inc.
+  </p>
+</div>
+</div>
+</Container>
+export default MyFooter
