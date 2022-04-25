79cc5eb01fefc3f255decf6acb9f11e7a67009e1
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-04-25 13:03:46 +0200

    navbar

--- my-react-app/src/components/MyNav.jsx
+++ my-react-app/src/components/MyNav.jsx
@@ -1,98 +1,62 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Dropdown, Nav } from "react-bootstrap";
+import netflix from "../Images/netflix_logo.png";
+import dropdown from "../Images/dropdown.png";
 
 class MyNav extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg navbar-dark dark-netflix">
-          <a class="navbar-brand" href="#">
-            <img
-              src="./assets/netflix_logo.png"
-              alt="netflix-logo"
-              width="145"
-              height="60"
-            />
+        <Navbar className="navbar navbar-expand-lg navbar-dark dark-netflix d-flex">
+          <a className="navbar-brand" href="#">
+            <img src={netflix} alt="netflix-logo" width="145" height="60" />
           </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Home <span class="sr-only">(current)</span>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">TV Shows</Nav.Link>
+              <Nav.Link href="#link">Movies</Nav.Link>
+              <Nav.Link href="#link">Recently Added</Nav.Link>
+              <Nav.Link href="#link">My List</Nav.Link>
+            </Nav>
+
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link"></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">KIDS</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">
+                  <i className="bi bi-bell"></i>
                 </a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="#">
-                  TV Shows
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link">Movies</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link">Recently Added</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link">My List</a>
               </li>
             </ul>
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link"></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link">KIDS</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link">
-                  <i class="bi bi-bell"></i>
-                </a>
-              </li>
-              <div class="dropdown">
-                <a
-                  class="btn dark-netflix dropdown-toggle"
-                  href="#"
-                  role="button"
-                  id="dropdownMenuLink"
-                  data-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <img
-                    src="./images/dropdown.png"
-                    alt="netflix-logo"
-                    width="30"
-                    height="30"
-                  />
-                </a>
 
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a class="dropdown-item" href="#">
-                    action 1
-                  </a>
-                  <a class="dropdown-item" href="#">
-                    Some
-                  </a>
-                </div>
-              </div>
-            </ul>
-          </div>
-        </nav>
+            <Dropdown>
+              <Dropdown.Toggle
+                style={{
+                  backgroundColor: "black",
+                  border: "none",
+                }}
+                id="dropdown-basic"
+                className="p-0"
+              >
+                <img src={dropdown} alt="netflix-logo" width="25" height="25" />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
